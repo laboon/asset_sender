3ae85b9d41abcb6853c541289a5f9d2e6b192c4d
laboon <laboon@gmail.com>
2022-04-02 11:41:12 +0200

    main work

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # asset_sender
-Mass Send Statemint/Statemine assets on Polkadot/Kusama
+Mass Send Statemine assets on Kusama
 
-*STILL UNDER DEVELOPMENT!*
+## Usage
+
+This will send out the existential deposit and a selected amount of a Statemine asset to a list of addresses included in a file.
+
+The file must include ONLY addresses, one per line.
+
+Note: You will need to enter your seed phrase for the account at the command line. This is, of course, very insecure. I recommend using a pass-through account which only stores the amount of assets and KSM to be sent out.
+
+### Command line arguments
+
+1. asset_id - The Statemine asset ID you wish to send
+2. amount_in_placks - The amount in Plancks you will send to each account. Note that different Statemine assets have different levels of precision!
+3. filename - The filename to read, which should contain a list of Kusama addresses, one per line
+4. seed_phrase_in_quotes - The seed phrase of the account from which a keypair will be generated and KSM/assets sent. Of course this account must already have some KSM and the asset you wish to send!
+
+### Example
+
+This example sends 100 Billcoins (asset ID 223) to the addresses found in filename.txt. Note that of course this is a dummy seed phrase; you will need to make your own Statemine account and fund it with KSM and the asset you wish to send.
+
+```
+python3 send.py 223 10000000000 filename.txt "end cry link reason crunch shine enroll supreme boost cluster fame devote"
+```
+
+## Usage on Statemint
+
+This is meant for STATEMINE, not STATEMINT, assets.
+
+To use on Statemint, change the STATEMINE_EXISTENTIAL_DEPOSIT and STATEMINE_NODE constants to the right Statemint values.
+
+Ideally, this would support Statemine/Statemint selection from the command line. PRs accepted.
+
+### Helpful Hint
+
+Use `cat file.txt | grep -o "[CDEFGHIJKLMN][a-zA-Z0-9]\{45\}[a-zA-Z0-9]*" | uniq` to get a list of all Kusama addresses in file.txt
+
+## Ideas for Improvement
+
+1. Statemine/Statemint selector
+2. Uses batch transactions (would also be much faster!)
+3. Check that account exists before sending ED; don't sent it if it does
+
+## Acknowledgements
+
+Thanks to Polkascan for building the [Python Substrate Interface](https://github.com/polkascan/py-substrate-interface), on which this is built.
--- send.py
+++ send.py
@@ -3,14 +3,21 @@ import sys
 from substrateinterface import SubstrateInterface, Keypair
 from substrateinterface.exceptions import SubstrateRequestException
 
+# Note that this is in Plancks.
+# The Statemine ED is 1/10th that of the Kusama ED.
+STATEMINE_EXISTENTIAL_DEPOSIT = 3333333
+STATEMINE_NODE = "wss://statemine-rpc.polkadot.io"
 
-def transfer_ksm(keypair, dest, amount):
+# NOTE - This is 100 Billcoins in Plancks.
+# 10000000000
+
+def transfer_ed_ksm(keypair, dest):
     call = substrate.compose_call(
         call_module='Balances',
         call_function='transfer',
         call_params={
-            'dest': 'JFArxqV6rqPSwBok3zQDnj5jL6vwsZQDwYXXqb1cFygnYVt',
-            'value': 1 * 10**6
+            'dest': dest,
+            'value': STATEMINE_EXISTENTIAL_DEPOSIT
         }
     )
 
@@ -24,7 +31,25 @@ def transfer_ksm(keypair, dest, amount):
         print("Failed to send: {}".format(e))
 
 def transfer_asset(keypair, asset_id, dest, amount):
-    print("tbd - transfer asset")
+    call = substrate.compose_call(
+        call_module='Assets',
+        call_function='transfer',
+        call_params={
+            'id': asset_id,
+            'target': dest,
+            'amount': amount
+        }
+    )
+
+    extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
+
+    try:
+        receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+        print("Extrinsic '{}' sent and included in block '{}'".format(receipt.extrinsic_hash, receipt.block_hash))
+
+    except SubstrateRequestException as e:
+        print("Failed to send: {}".format(e))
+
 
 ##########################################
 # EXECUTION STARTS HERE
@@ -33,16 +58,23 @@ def transfer_asset(keypair, asset_id, dest, amount):
 
 # Check that a seed phrase is entered
 
-if len(sys.argv) != 2:
-    sys.exit("Enter seed phrase in quotes")
+if len(sys.argv) != 5:
+    print("Arguments: asset_id amount_in_plancks filename seed_phrase_in_quotes")
+    sys.exit("Could not read arguments")
 
+print(sys.argv)
+asset_id = sys.argv[1]
+amount = sys.argv[2]
+filename = sys.argv[3]
+seed = sys.argv[4]
+    
 # Connect to node. By default this goes to Statemine, just change RPC endpoint to
-# Statemint to use that instead
+# Statemint to use that instead.
 
 print("Connecting to node...", end='')
 
 substrate = SubstrateInterface(
-    url="wss://statemine-rpc.polkadot.io"
+    url=STATEMINE_NODE
 )
 
 print(" done!")
@@ -50,55 +82,32 @@ print(" done!")
 # Generate keypair
 
 print("Generating keypair...", end='')
-    
-seed = sys.argv[1]
 
 keypair = Keypair.create_from_mnemonic(seed)
 
 print(" done!")
 
-transfer_ksm(keypair, "a", 1)
- 
-quit()
+print("Reading in addresses...", end='')
 
-print("Reading in addresses.txt...", end='')
-
-text_file = open("addresses.txt", "r")
+text_file = open(filename, "r")
 lines = text_file.readlines()
 text_file.close()
 
 print(" done!")
 
+num_addresses = 0
+
 for line in lines:
-        address = line.rstrip('\n')
-        print(address)
+    address = line.rstrip('\n')
+    if len(address) < 2:
+        break
+    print("Sending ED to " + address + "...")
+    transfer_ed_ksm(keypair, address)
+    print(" done!");
+    print("Sending " + amount + " of asset ID " + asset_id + " to " + address + "...")
+    transfer_asset(keypair, 223, address, 10000000000)
+    print(" done!")
+    num_addresses += 1
 
+print("Sent asset " + asset_id + " to " + str(num_addresses) + " accounts.")
 
-# # Set block_hash to None for chaintip
-# block_hash = None
-
-# # Retrieve extrinsics in block
-# result = substrate.get_block(block_hash=block_hash)
-
-# print(result)
-
-# for extrinsic in result['extrinsics']:
-
-#     if 'address' in extrinsic.value:
-#         signed_by_address = extrinsic.value['address']
-#     else:
-#         signed_by_address = None
-
-#     print('\nPallet: {}\nCall: {}\nSigned by: {}'.format(
-#         extrinsic.value["call"]["call_module"],
-#         extrinsic.value["call"]["call_function"],
-#         signed_by_address
-#     ))
-
-#     # Loop through call params
-#     for param in extrinsic.value["call"]['call_args']:
-
-#         if param['type'] == 'Balance':
-#             param['value'] = '{} {}'.format(param['value'] / 10 ** substrate.token_decimals, substrate.token_symbol)
-
-#         print("Param '{}': {}".format(param['name'], param['value']))
